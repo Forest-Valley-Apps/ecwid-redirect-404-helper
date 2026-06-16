31d1c476bad5206220563912e6c2a67559e85b7a
Sonnyko73 <sonny7373@gmail.com>
2026-06-16 23:12:09 +0100

    Harmonize the main-file License header to GPLv2 or later (match readme.txt)

--- redirect-404-helper-for-ecwid.php
+++ redirect-404-helper-for-ecwid.php
@@ -8,7 +8,7 @@
  * Requires PHP:      7.4
  * Author:            Forest Valley
  * Author URI:        https://apps.fv.dev/
- * License:           GPL v2 or later
+ * License:           GPLv2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       redirect-404-helper-for-ecwid
  * Domain Path:       /languages
